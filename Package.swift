f12d6be0ab95366af0b133b493404317e30aa0a1
Mathieu Tozer <tozer@mathieus-air.myfiosgateway.com>
2023-03-16 10:26:02 -0400

    Use remote repo

--- Package.swift
+++ Package.swift
@@ -18,12 +18,16 @@ let package = Package(
     dependencies: [
 ///        .package(name: "FilestackSDK", url: "https://github.com/filestack/filestack-swift", .upToNextMajor(from: Version(2, 7, 0))),
       .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.0"),
-      .package(name: "FilestackSDK", path: "../filestack-swift"),
+      .package(url: "https://github.com/mathieutozer/filestack-swift", branch: "master"),
+//      .package(name: "FilestackSDK", path: "../filestack-swift"),
     ],
     targets: [
         .target(
             name: "Filestack",
-            dependencies: ["FilestackSDK", "ZIPFoundation"],
+            dependencies: [
+              .product(name: "FilestackSDK", package: "filestack-swift"),
+              "ZIPFoundation"
+            ],
             exclude: ["Filestack.h", "Info.plist"],
             resources: [
                 .copy("VERSION")
